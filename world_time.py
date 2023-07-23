c493429135dbe68df45c79581e212bd43c185e11
luiscx99 <themasterhex@gmail.com>
2023-07-22 20:33:19 -0400

    bug fixes

--- world_time.py
+++ world_time.py
@@ -4,7 +4,7 @@ from ttkwidgets.autocomplete import AutocompleteEntry
 from zonedict import allzone as ct
 from datetime import datetime
 from PIL import Image, ImageTk
-import random 
+import random
 import pytz
 
 
@@ -17,33 +17,38 @@ class App(ttk.Window):
         ttk.Style("yeti")
         self.title(title)
         self.geometry(f'{size[0]}x{size[1]}')
-        self.minsize(size[0],size[1])
-        self.maxsize(size[0],size[1])
-       
+        self.minsize(size[0], size[1])
+        self.maxsize(size[0], size[1])
+
         # title_frame
         self.titlelable = Title_frame(self)
         # input_fram
         self.inputframe = Input_aframe(self)
         # output local
-        self.outputlocal = Local_time(self, 'The local time is:', 'Calibri 9 bold', 'Calibri 10 bold')
+        self.outputlocal = Local_time(
+            self, 'The local time is:', 'Calibri 9 bold', 'Calibri 10 bold')
         # output txt
-        self.outputalltext = Output_txtframe(self, 'info', 'inverse-info', 'Calibri 11 bold', 'Calibri 15 bold', '#258fac')
+        self.outputalltext = Output_txtframe(
+            self, 'info', 'inverse-info', 'Calibri 11 bold', 'Calibri 15 bold', '#258fac')
         # time frame
-        self.timeframe = Time_frame(self, 'info', 'inverse-info', 'Calibri 50 bold')
+        self.timeframe = Time_frame(
+            self, 'info', 'inverse-info', 'Calibri 50 bold')
         # Setting app icon
-        icon = get_styleimg('World-Time/GUI/world_time_icon.ico', 256, 256)
+        icon = get_styleimg('GUI/world_time_icon.ico', 256, 256)
         self.wm_iconphoto(False, icon)
-        
+
         self.mainloop()
 
- 
-# import all time zone dictionary
+
+# import time zone dictionary
 all_zone = ct.zone_dict()
 
 TXT_T = 'This is the current time in:'
 backup_country = {}
 
 # fix country input to title case
+
+
 def country_title_c():
     inputstr = entry_str.get()
     if inputstr in all_zone:
@@ -53,6 +58,8 @@ def country_title_c():
     return strinput
 
 # Get country time
+
+
 def get_time():
     global country_tz, current_time_in, TXT_T
     check_error(0)
@@ -64,7 +71,9 @@ def get_time():
 
     output_ctime()
 
-# set country time 
+# set country time
+
+
 def output_ctime():
     output_str.set(country_title_c())
     output_txt.set(TXT_T)
@@ -73,15 +82,19 @@ def output_ctime():
     upd_localtime()
 
 # local time
+
+
 def upd_localtime():
-#    local = pydatetime()
+    #    local = pydatetime()
     global current_day
     current_day = The_time(False).day_time("%m/%d/%Y, %I:%M %p")
     output_local_time_str.set(current_day)
 # return to Local_time
     return current_day
 
-# set default time 
+# set default time
+
+
 def default_time():
     default_zone, country_zone = random.choice(list(all_zone.items()))
  #   save_zone = The_time(country_zone)
@@ -92,19 +105,22 @@ def default_time():
     output_timestr.set(default_time_in)
     output_txt.set(TXT_T)
 
+
 def setup_error_frame(error: bool):
     if error:
-            emptyframe.pack_forget()
-            errorLable.pack(before=input_entry)
-            entry_str.set('')
+        emptyframe.pack_forget()
+        errorLable.pack(before=input_entry)
+        entry_str.set('')
     elif not error:
-            errorLable.pack_forget()
-            emptyframe.pack(before=input_entry)
-            
+        errorLable.pack_forget()
+        emptyframe.pack(before=input_entry)
+
 # check for empty or wrong country
+
+
 def check_error(val: bool):
     if not entry_str.get() and not val:
-            setup_error_frame(True)
+        setup_error_frame(True)
     elif entry_str.get() and not val:
         strinput = country_title_c()
         if strinput in all_zone:
@@ -112,12 +128,15 @@ def check_error(val: bool):
         else:
             setup_error_frame(True)
 
-# get backgroud image
+# get images
+
+
 def get_styleimg(img: str, wdth: int, hght: int):
     image = Image.open(img)
     image = image.resize((wdth, hght))
     image = ImageTk.PhotoImage(image)
     return image
+
 
 class The_time:
     def __init__(self, zone):
@@ -128,18 +147,19 @@ class The_time:
             self.pydatetime = datetime.now()
 
     def day_time(self, local: str):
-#        if not local:
-            timein = self.pydatetime.strftime(local)
-#        elif local:
-#            localtime = self.local_time.strftime("%m/%d/%Y, %I:%M %p")
-            return timein
+        timein = self.pydatetime.strftime(local)
+        return timein
+
 
 class Title_frame(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        ttk.Label(self, text='World Time', font='Calibri 24 bold').pack(pady=10)
-        ttk.Label(self, text='Get the time from any country in the world', font='Calibri 10 bold').pack()
+        ttk.Label(self, text='World Time',
+                  font='Calibri 24 bold').pack(pady=10)
+        ttk.Label(self, text='Get the time from any country in the world',
+                  font='Calibri 10 bold').pack()
         self.pack()
+
 
 class Input_aframe(ttk.Frame):
     def __init__(self, master):
@@ -147,20 +167,23 @@ class Input_aframe(ttk.Frame):
         self.pack_propagate(False)
         self.pack(padx=50)
         self.input_all()
-        
+
     def input_all(self):
         complete_zone = list(all_zone.keys())
         global entry_str, input_entry, errorLable, emptyframe
         entry_str = tk.StringVar()
         # auto complete entry
-        input_entry = AutocompleteEntry(self,textvariable=entry_str, completevalues=complete_zone, font='Calibri 10 bold')
-        input_entry.bind('<Return>', lambda event:get_time())
+        input_entry = AutocompleteEntry(
+            self, textvariable=entry_str, completevalues=complete_zone, font='Calibri 10 bold')
+        input_entry.bind('<Return>', lambda event: get_time())
         input_button = ttk.Button(self, text='Get Time', command=get_time)
         emptyframe = ttk.Frame(self, width=30, height=19)
-        errorLable = ttk.Label(self, text='Type the correct timezone or country', font='Calibri 10 bold', foreground='red')
+        errorLable = ttk.Label(
+            self, text='Type the correct timezone or country', font='Calibri 10 bold', foreground='red')
         emptyframe.pack()
         input_entry.pack(padx=5, side='left')
         input_button.pack(padx=5, side='left')
+
 
 class Local_time(ttk.Frame):
     def __init__(self, master, Ltext: str, Lfont: str, timef: str):
@@ -170,11 +193,13 @@ class Local_time(ttk.Frame):
         output_local_time_str = tk.StringVar()
         output_local_time_str.set(upd_localtime())
         localttx = ttk.Label(self, text=Ltext, font=Lfont)
-        localtimetxt = ttk.Label(self, textvariable=output_local_time_str, font=timef)
+        localtimetxt = ttk.Label(
+            self, textvariable=output_local_time_str, font=timef)
         localttx.pack(side='top', fill='x')
         localtimetxt.pack(side='top', fill='x')
         self.pack(padx=7)
-       
+
+
 class Output_txtframe(ttk.Frame):
     def __init__(self, master, style: str, invstyle: str, fontstr: str, fonttxt: str, fg: str):
         super().__init__(master, bootstyle=style, width=450, height=46)
@@ -183,29 +208,34 @@ class Output_txtframe(ttk.Frame):
         output_txt = tk.StringVar()
         output_str = tk.StringVar()
 
-        self.image = get_styleimg("World-Time/outputtext.png", 450, 23)
-        self.image2 = get_styleimg("World-Time/outputtext2.png", 450, 23)
+        self.image = get_styleimg("outputtext.png", 450, 23)
+        self.image2 = get_styleimg("outputtext2.png", 450, 23)
 
-        ttk.Label(self, border='0', image=self.image, bootstyle='cerculean', textvariable=output_txt, font=fontstr, compound='center').pack(expand=True, side='top')
-        ttk.Label(self, border='0', image=self.image2, bootstyle=invstyle, textvariable=output_str, font=fonttxt, foreground=fg, compound='center').pack(expand=True, side='top')
-        self.pack()  
+        ttk.Label(self, border='0', image=self.image, bootstyle='cerculean',
+                  textvariable=output_txt, font=fontstr, compound='center').pack(expand=True, side='top')
+        ttk.Label(self, border='0', image=self.image2, bootstyle=invstyle, textvariable=output_str,
+                  font=fonttxt, foreground=fg, compound='center').pack(expand=True, side='top')
+        self.pack()
+
 
 class Time_frame(ttk.Frame):
     def __init__(self, master, stylet: str, invtext: str, tfont: str):
         super().__init__(master, bootstyle=stylet, width=450, height=190)
         self.pack_propagate(False)
         global output_timestr
-        self.image = get_styleimg("World-Time/timezone.png", 450, 199)
-        
+        self.image = get_styleimg("timezone.png", 450, 190)
+
         output_timestr = tk.StringVar()
-        ttk.Label(self, border='0', image=self.image, textvariable=output_timestr, font=tfont, compound='center', foreground='#126D90').pack(ipadx='5', side='top')
+        ttk.Label(self, border='0', image=self.image, textvariable=output_timestr,
+                  font=tfont, compound='center', foreground='#126D90').pack(ipadx='5', side='top')
         self.pack()
 
         self.after(600, self.update_time_frame)
         # update time
+
     def update_time_frame(self):
         if backup_country == {}:
-        # default time
+            # default time
             default_time()
         else:
             get_timeIn = The_time(backup_country[1])
@@ -215,4 +245,5 @@ class Time_frame(ttk.Frame):
 
         self.after(60000, self.update_time_frame)
 
-App('World Time', (450,407))
+
+App('World Time', (450, 437))
